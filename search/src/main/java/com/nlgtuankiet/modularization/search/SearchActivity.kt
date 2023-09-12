f5937fffe014929cfed5f16f974a84232266ed8d
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2023-09-12 17:38:21 +0700

    upgrade to android gradle plugin 8.1.0

--- search/src/main/java/com/nlgtuankiet/modularization/search/SearchActivity.kt
+++ search/src/main/java/com/nlgtuankiet/modularization/search/SearchActivity.kt
@@ -1,15 +1,14 @@
 package com.nlgtuankiet.modularization.search
 
+
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import com.nlgtuankiet.modularization.common.StarterIntents
 import dagger.android.support.DaggerAppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_search.*
 import javax.inject.Inject
 
 class SearchActivity : DaggerAppCompatActivity() {
@@ -17,12 +16,12 @@ class SearchActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         val keyword = intent.getStringExtra("keyword") ?: ""
         findViewById<EditText>(R.id.editText).setText(keyword)
 
-        fab.setOnClickListener { view ->
+        findViewById<View>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
